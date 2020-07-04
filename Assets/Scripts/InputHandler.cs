2925ad2ea6fcaa07f0c49871d551214f9543221b
gaps <gaps@no.email>
2020-07-04 20:55:49 +0100

    Throw a ball that destroys a block instead of destroying it immediately

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -5,8 +5,12 @@ public class InputHandler : MonoBehaviour, IPointerDownHandler, IPointerUpHandle
 {
     public CameraController cameraController;
 
+    public GameObject ballPrefab;
+
     [Tooltip("Corresponds to the angle in degrees that the camera will rotate upon a drag from one edge of the screen to the other.")]
     public float cameraRotationSpeed = 180;
+
+    public float throwSpeed = 35;
 
     private bool dragging = false;
     private float cameraAngle = 0;
@@ -46,7 +50,43 @@ public class InputHandler : MonoBehaviour, IPointerDownHandler, IPointerUpHandle
         Ray ray = cameraController.gameCamera.ScreenPointToRay(eventData.position);
         if (Physics.Raycast(ray, out RaycastHit hitInfo))
         {
-            hitInfo.transform.GetComponentInParent<Block>()?.Explode();
+            Block block = hitInfo.transform.GetComponentInParent<Block>();
+            if (block != null)
+            {
+                ThrowBall(block, hitInfo.point);
+            }
         }
+    }
+
+    private async void ThrowBall(Block block, Vector3 hitPosition)
+    {
+        float gravity = Physics.gravity.y;
+
+        Vector3 initialBallPosition = cameraController.gameCamera.transform.position;
+        GameObject ball = Instantiate(ballPrefab, initialBallPosition, Quaternion.identity);
+
+        Vector3 distance = hitPosition - initialBallPosition;
+        Vector2 horizontalDistance = new Vector2(distance.x, distance.z);
+        float totalTime = horizontalDistance.magnitude / throwSpeed;
+        float verticalSpeed = distance.y / totalTime - gravity * totalTime * 0.5f;
+
+        float startTime = Time.time;
+        float endTime = startTime + totalTime;
+        Vector2 horizontalDirection = horizontalDistance.normalized;
+
+        await TaskUtils.WaitForNextUpdate(this);
+        while (Time.time < endTime)
+        {
+            float t = Time.time - startTime;
+            float verticalDiff = (gravity * t * 0.5f + verticalSpeed) * t;
+            Vector2 horizontalDiff = horizontalDirection * (throwSpeed * t);
+
+            ball.transform.position = initialBallPosition + new Vector3(horizontalDiff.x, verticalDiff, horizontalDiff.y);
+
+            await TaskUtils.WaitForNextUpdate(this);
+        }
+
+        Destroy(ball);
+        block.Explode();
     }
 }
