51c057365f23e41eaed7a53db3c9a42faae412f7
gaps <gaps@no.email>
2020-07-04 16:42:38 +0100

    Rotate the camera when click and dragging the pointer

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Camera gameCamera;
+
+    public Vector3 lookAtPosition;
+    public float distanceOffset = 20;
+    public float heightOffset = 8;
+
+    private void Start()
+    {
+        SetRotationAngle(0);
+    }
+
+    public void SetRotationAngle(float angle)
+    {
+        transform.position = lookAtPosition +
+            (Quaternion.Euler(0, angle, 0) * new Vector3(0, heightOffset, -distanceOffset));
+        transform.LookAt(lookAtPosition);
+    }
+}
---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InputHandler : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
+{
+    public CameraController cameraController;
+
+    [Tooltip("Corresponds to the angle in degrees that the camera will rotate upon a drag from one edge of the screen to the other.")]
+    public float cameraRotationSpeed = 180;
+
+    private bool dragging = false;
+    private float cameraAngle = 0;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        // It's only needed so that the OnPointerUp works
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (dragging)
+        {
+            cameraAngle = (cameraAngle + CalcCameraDeltaAngle(eventData)) % 360;
+            cameraController.SetRotationAngle(cameraAngle);
+            dragging = false;
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        dragging = true;
+        cameraController.SetRotationAngle(cameraAngle + CalcCameraDeltaAngle(eventData));
+    }
+
+    public float CalcCameraDeltaAngle(PointerEventData eventData)
+    {
+        return (eventData.position.x - eventData.pressPosition.x) / Screen.width * cameraRotationSpeed;
+    }
+}
