22163ff9a4532640c94c02d3bbd17f53a19be304
gaps <gaps@no.email>
2020-07-04 20:51:13 +0100

    Task utilities that allow asynchronously awaiting for Unity yield instructions

---
+++ Assets/Scripts/Utils/TaskUtils.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public static class TaskUtils
+{
+    public static Task WaitForSeconds(MonoBehaviour target, float seconds)
+    {
+        return WaitForYieldInstruction(target, new WaitForSeconds(seconds));
+    }
+
+    public static Task WaitForSecondsRealtime(MonoBehaviour target, float seconds)
+    {
+        return WaitForYieldInstruction(target, new WaitForSecondsRealtime(seconds));
+    }
+
+    public static Task WaitForNextUpdate(MonoBehaviour target)
+    {
+        return WaitForYieldInstruction(target, null);
+    }
+
+    public static Task WaitForYieldInstruction(MonoBehaviour target, object yieldInstruction)
+    {
+        TaskCompletionSource<bool> taskSource = new TaskCompletionSource<bool>();
+        target.StartCoroutine(WaitForSecondsAux(yieldInstruction, taskSource));
+        return taskSource.Task;
+    }
+
+    private static IEnumerator WaitForSecondsAux(object yieldInstruction, TaskCompletionSource<bool> taskSource)
+    {
+        yield return yieldInstruction;
+        taskSource.SetResult(true);
+    }
+}
