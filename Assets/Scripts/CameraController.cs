96a1e39fab42690d27d36d49169b9e35dc43d478
gaps <gaps@no.email>
2020-07-06 20:33:36 +0100

    Animate the camera movement when moving to another tower level

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    private const float DefaultNextTowerLevelMovementDuration = 0.25f;
+
     public Camera gameCamera;
 
     public Vector3 lookAtPosition;
@@ -11,8 +13,10 @@ public class CameraController : MonoBehaviour
     public float rotationAngle = 0;
     [Tooltip("Corresponds to the angle in degrees that the camera will rotate upon a drag from one edge of the screen to the other.")]
     public float rotationSpeed = 180;
-    [Tooltip("Corresponds to the speed when moving to another tower level position.")]
-    public float nextTowerLevelSpeed = 5;
+    [Tooltip("Corresponds to the movement curve when moving from a tower level to the one below.")]
+    public AnimationCurve nextTowerLevelMovementCurve = new AnimationCurve(
+        new Keyframe(0, 0, 0, 1 / DefaultNextTowerLevelMovementDuration),
+        new Keyframe(DefaultNextTowerLevelMovementDuration, 1, 0, 0));
 
     private void Start()
     {
@@ -31,13 +35,38 @@ public class CameraController : MonoBehaviour
         UpdateTransform();
     }
 
-    public async void MoveToLevelAtPosition(Vector3 lookAtPosition)
+    public async void MoveToLevelAtPosition(Vector3 newLookAtPosition)
     {
-        // TODO: Start an animation and wait for it to end
-        float duration = Mathf.Abs(this.lookAtPosition.y - lookAtPosition.y) / nextTowerLevelSpeed;
-        await TaskUtils.WaitForSecondsRealtime(this, duration);
+        Vector3 deltaLookAtPosition = newLookAtPosition - lookAtPosition;
+        Vector3 startLookAtPosition = lookAtPosition;
 
-        this.lookAtPosition = lookAtPosition;
+        float curveDuration = nextTowerLevelMovementCurve.length > 0
+            ? nextTowerLevelMovementCurve.keys[nextTowerLevelMovementCurve.length - 1].time
+            : 0;
+        if (curveDuration > 0)
+        {
+            float durationMultiplier = Mathf.Abs(deltaLookAtPosition.y);
+            float invDurationMultiplier = 1 / durationMultiplier;
+            float startTime = Time.unscaledTime;
+            float endTime = startTime + curveDuration * durationMultiplier;
+
+            while (true)
+            {
+                await TaskUtils.WaitForNextUpdate(this);
+                float currentTime = Time.unscaledTime;
+                if (currentTime >= endTime)
+                {
+                    break;
+                }
+
+                float deltaTime = (currentTime - startTime) * invDurationMultiplier;
+                float value = nextTowerLevelMovementCurve.Evaluate(deltaTime);
+                lookAtPosition = startLookAtPosition + deltaLookAtPosition * value;
+                UpdateTransform();
+            }
+        }
+
+        lookAtPosition = startLookAtPosition + deltaLookAtPosition;
         UpdateTransform();
     }
 
